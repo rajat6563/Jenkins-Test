969e645154c37c0b79555252318ff913a85a93d0
Rajat Yadav <rajat.yadav@DARCL.CORP>
2017-07-17 17:33:30 +0530

    created 2 test cases

--- test/test.js
+++ test/test.js
@@ -7,11 +7,20 @@ var server = supertest.agent("http://localhost:4000");
 
 // UNIT test begin
 
-describe('Array', function() {
+describe('Array1', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(1).should.equal(-1);
+      [1,2,3].indexOf(0).should.equal(-1);
+    });
+  });
+});
+
+describe('Array2', function() {
+  describe('#indexOf()', function() {
+    it('should return index for array when the value is present', function() {
+      [1,2,3].indexOf(1).should.equal(0);
+      [1,2,3].indexOf(2).should.equal(1);
     });
   });
 });
