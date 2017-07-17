93d52c9eda83a2c49ba1bb7a4b0f7165e4830a80
Rajat Yadav <rajat.yadav@DARCL.CORP>
2017-07-17 16:11:43 +0530

    changes by rajat in appjs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World of Jenkins by Rajat Yadav");
+  res.send("Hello World of Jenkins by Rajat");
 });
 
 app.listen(4000);
