42e51cb373a0c7577e204acd400a67a342df1359
Rajat Yadav <rajat.yadav@DARCL.CORP>
2017-07-18 11:30:15 +0530

    changes in appjs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World of Jenkins and test case run by Rajat Yv");
+  res.send("Hello World Jenkins");
 });
 
 app.listen(4000);
