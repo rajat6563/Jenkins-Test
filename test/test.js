c2826b930d5122ab49110c00ad3eb3ade965c57c
Rajat Yadav <rajat.yadav@DARCL.CORP>
2017-07-24 13:28:41 +0530

    changes in dev branch

--- test/test.js
+++ test/test.js
@@ -15,12 +15,3 @@ describe('Array1', function() {
     });
   });
 });
-
-describe('Array2', function() {
-  describe('#indexOf()', function() {
-    it('should check if value is present in array', function() {
-      [1,2,3].indexOf(1).should.equal(0);
-      [1,2,3].indexOf(2).should.equal(1);
-    });
-  });
-});
