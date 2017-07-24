74b0375dbd9faaec1ee21615684c32fdcdcf4008
Rajat Yadav <rajat.yadav@DARCL.CORP>
2017-07-24 16:24:34 +0530

    sadsfdsf

--- test/test.js
+++ test/test.js
@@ -24,12 +24,3 @@ describe('Array2', function() {
     });
   });
 });
-
-describe('Array3', function() {
-  describe('#indexOf()', function() {
-    it('should check if value is present in array', function() {
-      [1,2,3].indexOf(1).should.equal(0);
-      [1,2,3].indexOf(2).should.equal(1);
-    });
-  });
-});
