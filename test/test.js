13fdd0c66c57431412763c239d248c448a465eab
Rajat Yadav <rajat.yadav@DARCL.CORP>
2017-07-24 16:35:48 +0530

    changes in testcase according to dev

--- test/test.js
+++ test/test.js
@@ -24,3 +24,12 @@ describe('Array2', function() {
     });
   });
 });
+
+describe('Array3', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      [1,2,3].indexOf(5).should.equal(-1);
+      [1,2,3].indexOf(0).should.equal(-1);
+    });
+  });
+});
