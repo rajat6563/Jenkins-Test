afd40f9639dc58834501e19ac9c4a21a43b12e37
Rajat Yadav <rajat.yadav@DARCL.CORP>
2017-07-28 16:41:40 +0530

    changes for npm start

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hurrayyyy... So we have successfully done Continuous Deployment from Dev Branch to Master Branch");
+  res.send("Congrats.... we have successfully done Continuous Deployment from Dev Branch to Master Branch");
 });
 
 app.listen(4000);
